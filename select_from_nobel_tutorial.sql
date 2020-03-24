fa7f13578fe48c21cac08d375ddb9ffed4676d3d
Oscar Mier <voscarmv@gmail.com>
2020-03-23 22:55:03 -0600

    Made progress on 6.

--- select_from_nobel_tutorial.sql
+++ select_from_nobel_tutorial.sql
@@ -129,3 +129,4 @@ order by
   end,
   subject,
   winner
+  
---
+++ the_join_operation.sql
@@ -0,0 +1,66 @@
+select
+  game.id,
+  goal.player
+from
+  game inner join goal
+where
+  game.id = goal.matchid and
+  goal.teamid = 'GER'
+
+select
+  game.stadium,
+  game.team1,
+  game.team2
+from
+  game
+where
+  game.id = 1012
+
+select
+  player,
+  teamid,
+  stadium,
+  mdate
+from
+  game join goal on (
+    id=matchid and
+    teamid='GER'
+  )
+
+select
+  team1,
+  team2,
+  player
+from
+  game join goal on (
+    id=matchid and
+    player like 'Mario%'
+  )
+
+select
+  player,
+  teamid,
+  coach,
+  gtime
+from
+  goal join eteam on (
+    goal.teamid = eteam.id and
+    gtime <= 10
+  )
+
+select
+  mdate,
+  teamname
+from
+  game join eteam on (
+    team1 = eteam.id and
+    coach = 'Fernando Santos'
+  )
+
+select
+  player
+from
+  goal join game on (
+    goal.matchid = game.id and
+    stadium = 'National Stadium, Warsaw'
+  )
