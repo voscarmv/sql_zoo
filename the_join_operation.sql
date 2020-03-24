6333bf7b7e42cc5acf76cc946d4dcbaf846ad5bc
Oscar Mier <voscarmv@gmail.com>
2020-03-24 08:40:38 -0600

    Finished 6 JOIN.

--- the_join_operation.sql
+++ the_join_operation.sql
@@ -73,3 +73,78 @@ from
 where
   (team1='GER' or team2='GER') and
   goal.teamid <> 'GER'
+
+select
+  teamname,
+  sum(1)
+from
+  goal join eteam on(
+    teamid = id
+  )
+group by
+  teamname
+
+select
+  stadium,
+  sum(1)
+from
+  goal join game on (
+    id = matchid
+  )
+group by
+  stadium
+
+select
+  matchid,
+  mdate,
+  sum(1)
+from
+  goal join game on 
+    id = matchid
+where
+  team1 = 'POL' or team2 = 'POL'
+group by
+  matchid
+
+select
+  matchid,
+  mdate,
+  sum(1)
+from
+  goal join game on 
+    id = matchid
+where
+  (team1 = 'GER' or team2 = 'GER') and
+  teamid = 'GER'
+group by
+  matchid
+
+select
+  mdate,
+  team1,
+  sum(
+    case
+      when teamid = team1 then 1
+      when teamid = team2 then 0
+      else 0
+    end
+  ) score1,
+  team2,
+  sum(
+    case
+      when teamid = team1 then 0
+      when teamid = team2 then 1
+      else 0
+    end
+  ) score2
+from
+  game left join goal on (
+    id = matchid
+  )
+group by
+  id
+order by
+  mdate,
+  matchid,
+  team1,
+  team2
